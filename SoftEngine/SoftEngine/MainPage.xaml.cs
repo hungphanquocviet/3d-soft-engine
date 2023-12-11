0e55a1d2ac5bdb73cfb295810f48c1a0f91e4e27
hungphanquocviet <hungphanquocviet@gmail.com>
2023-12-10 21:45:52 -0700

    New changes

--- SoftEngine/SoftEngine/MainPage.xaml.cs
+++ SoftEngine/SoftEngine/MainPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using SharpDX;
 using Windows.UI.Xaml.Media.Imaging;
+using System.Collections.ObjectModel;
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace SoftEngine
@@ -24,47 +25,21 @@ namespace SoftEngine
     public partial class MainPage : Page
     {
         private Device device;
-        Mesh mesh = new Mesh("Cube", 8, 12);
+        Mesh[] meshes;
         Camera mera = new Camera();
-        public MainPage()
-        {
-            this.InitializeComponent();
+        DateTime previousDate;
+        private Collection<double> lastFPSValues = new Collection<double>();
 
-        }
-
-        private void Page_Loaded(object sender, RoutedEventArgs e)
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
             // Choose the back buffer resolution here
-            WriteableBitmap bmp = new WriteableBitmap(640, 480);
+            WriteableBitmap bmp = new WriteableBitmap((int)ActualWidth, (int)ActualHeight);
 
-            device = new Device(bmp);
-
-            // Our XAML Image control
+            // Our Image XAML control
             frontBuffer.Source = bmp;
 
-            mesh.Vertices[0] = new Vector3(-1, 1, 1);
-            mesh.Vertices[1] = new Vector3(1, 1, 1);
-            mesh.Vertices[2] = new Vector3(-1, -1, 1);
-            mesh.Vertices[3] = new Vector3(1, -1, 1);
-            mesh.Vertices[4] = new Vector3(-1, 1, -1);
-            mesh.Vertices[5] = new Vector3(1, 1, -1);
-            mesh.Vertices[6] = new Vector3(1, -1, -1);
-            mesh.Vertices[7] = new Vector3(-1, -1, -1);
-
-            mesh.Faces[0] = new Face { A = 0, B = 1, C = 2 };
-            mesh.Faces[1] = new Face { A = 1, B = 2, C = 3 };
-            mesh.Faces[2] = new Face { A = 1, B = 3, C = 6 };
-            mesh.Faces[3] = new Face { A = 1, B = 5, C = 6 };
-            mesh.Faces[4] = new Face { A = 0, B = 1, C = 4 };
-            mesh.Faces[5] = new Face { A = 1, B = 4, C = 5 };
-
-            mesh.Faces[6] = new Face { A = 2, B = 3, C = 7 };
-            mesh.Faces[7] = new Face { A = 3, B = 6, C = 7 };
-            mesh.Faces[8] = new Face { A = 0, B = 2, C = 7 };
-            mesh.Faces[9] = new Face { A = 0, B = 4, C = 7 };
-            mesh.Faces[10] = new Face { A = 4, B = 5, C = 6 };
-            mesh.Faces[11] = new Face { A = 4, B = 6, C = 7 };
-
+            device = new Device(bmp);
+            meshes = await device.LoadJSONFileAsync("monkey.babylon");
             mera.Position = new Vector3(0, 0, 10.0f);
             mera.Target = Vector3.Zero;
 
@@ -75,15 +50,58 @@ namespace SoftEngine
         // Rendering loop handler
         void CompositionTarget_Rendering(object sender, object e)
         {
+            // Fps
+            var now = DateTime.Now;
+            var currentFPS = 1000.0 / (now - previousDate).TotalMilliseconds;
+            previousDate = now;
+
+            fps.Text = string.Format("instant {0:0.00} fps", currentFPS);
+
+            if (lastFPSValues.Count < 60)
+            {
+                lastFPSValues.Add(currentFPS);
+            }
+            else
+            {
+                lastFPSValues.RemoveAt(0);
+                lastFPSValues.Add(currentFPS);
+                var totalValues = 0d;
+                for (var i = 0; i < lastFPSValues.Count; i++)
+                {
+                    totalValues += lastFPSValues[i];
+                }
+
+                var averageFPS = totalValues / lastFPSValues.Count;
+                averageFps.Text = string.Format("average {0:0.00} fps", averageFPS);
+            }
+
             device.Clear(0, 0, 0, 255);
 
-            // rotating slightly the cube during each frame rendered
-            mesh.Rotation = new Vector3(mesh.Rotation.X + 0.01f, mesh.Rotation.Y + 0.01f, mesh.Rotation.Z);
+            foreach (var mesh in meshes)
+            {
+                // rotating slightly the meshes during each frame rendered
+                //mesh.Rotation = new Vector3(mesh.Rotation.X + 0.01f, mesh.Rotation.Y + 0.01f, mesh.Rotation.Z);
+                mesh.Rotation = new Vector3(mesh.Rotation.X, mesh.Rotation.Y + 0.01f, mesh.Rotation.Z);
+            }
 
             // Doing the various matrix operations
-            device.Render(mera, mesh);
+            device.Render(mera, meshes);
             // Flushing the back buffer into the front buffer
             device.Present();
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
         }
 
     }
